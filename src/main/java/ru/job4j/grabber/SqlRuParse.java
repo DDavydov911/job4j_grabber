af7861ef52e18dce3d8265393bec6e04dc0f13ea
Denis Davydov <dvd9@yandex.ru>
2021-11-16 20:44:52 +0300

    several moving

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -4,53 +4,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.Parse;
-import ru.job4j.grabber.Post;
-import ru.job4j.grabber.DateTimeParser;
-import ru.job4j.grabber.SqlRuDateTimeParser;
-
-import java.io.IOException;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import java.time.LocalDateTime;
-import java.util.List;
 
-public class SqlRuParse implements Parse {
-    private final DateTimeParser dateTimeParser;
+public class SqlRuParse {
 
-    public SqlRuParse(DateTimeParser dateTimeParser) {
-        this.dateTimeParser = dateTimeParser;
-    }
-
-    @Override
-    public List<Post> list(String link) {
-        return null;
-    }
-
-    @Override
-    public Post detail(String link) {
-        Post post = null;
-        /*
-        Element href = element.child(0);
-        String line = href.attr("href");
-        String title = href.text();
-        LocalDateTime ldt = dateTimeParser.parse(element.parent().child(5).text());
-        Document innerDoc = null;
-        try {
-            innerDoc = Jsoup.connect(href.attr("href")).get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Elements innerRow = innerDoc.select(".msgBody").next();
-        StringBuilder description = new StringBuilder();
-        for (Element el : innerRow) {
-            description.append(el.text());
-        }
-        post = new Post(title, line, description.toString(), ldt);
-
-         */
-        return post;
-    }
-
-    private String getDescription(Elements innerRow) {
+    private static String getDescription(Elements innerRow) {
         StringBuilder description = new StringBuilder();
         for (Element el : innerRow) {
             description.append(el.text());
@@ -59,7 +18,6 @@ public class SqlRuParse implements Parse {
     }
 
     public static void main(String[] args) throws Exception {
-        SqlRuDateTimeParser dateTimeParser = new SqlRuDateTimeParser();
         for (int i = 1; i < 2; i++) {
             SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
@@ -71,11 +29,8 @@ public class SqlRuParse implements Parse {
                 LocalDateTime ldt = parser.parse(td.parent().child(5).text());
                 Document innerDoc = Jsoup.connect(href.attr("href")).get();
                 Elements innerRow = innerDoc.select(".msgBody").next();
-                StringBuilder description = new StringBuilder();
-                for (Element el : innerRow) {
-                    description.append(el.text());
-                }
-                Post post = new Post(title, link, description.toString(), ldt);
+                String description = getDescription(innerRow);
+                Post post = new Post(title, link, description, ldt);
                 System.out.println(post);
             }
         }
