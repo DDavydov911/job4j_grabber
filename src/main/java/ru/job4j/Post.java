265621669f8059d2f816fc92e63cc8e566fc9894
Denis Davydov <dvd9@yandex.ru>
2021-11-16 12:29:07 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/Post.java
@@ -0,0 +1,38 @@
+package ru.job4j;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    int id;
+    String title;
+    String link;
+    String description;
+    LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(title, post.title)
+                && Objects.equals(link, post.link)
+                && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title
+                + '\'' + ", link='" + link
+                + '\'' + ", description='" + description
+                + '\'' + ", created=" + created
+                + '}';
+    }
+}
