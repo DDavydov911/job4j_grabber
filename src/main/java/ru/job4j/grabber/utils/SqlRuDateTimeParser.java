c6686618e6b28a36e18a7f024b84b5d8ffc0c695
Denis Davydov <dvd9@yandex.ru>
2021-11-16 12:01:25 +0300

    2.1. Преобразование даты [#289476] checkstyle

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class SqlRuDateTimeParser implements DateTimeParser {
 
     private static final Map<String, String> MONTHS = Map.ofEntries(Map.entry("янв", "01"),
-        Map.entry("фев", "02"), Map.entry("мар", "03") , Map.entry("апр", "04"),
+        Map.entry("фев", "02"), Map.entry("мар", "03"), Map.entry("апр", "04"),
         Map.entry("май", "05"), Map.entry("июн", "06"), Map.entry("июл", "07"),
         Map.entry("авг", "08"), Map.entry("сен", "09"), Map.entry("окт", "10"),
         Map.entry("ноя", "11"), Map.entry("дек", "12"));
@@ -15,9 +15,9 @@ public class SqlRuDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
         String[] arr = parse.split(" |, |:");
-        DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd MM yy HH mm");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("ddMMyyHHmm");
         return LocalDateTime.parse(
-                arr[0] + MONTHS.get(arr[1]) + arr[2] + arr[3] + arr[4], DATE_FORMAT
+                arr[0] + MONTHS.get(arr[1]) + arr[2] + arr[3] + arr[4], dateTimeFormatter
         );
     }
 }
