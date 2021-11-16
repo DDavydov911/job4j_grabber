5764dc671970dc678a2624763aa4de00131103fb
Denis Davydov <dvd9@yandex.ru>
2021-11-16 17:54:40 +0300

    2.2. Модель данных - Post. [#285211 #229084] fixed

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.grabber;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -19,21 +19,15 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Post post = (Post) o;
-        return id == post.id && Objects.equals(title, post.title)
-                && Objects.equals(link, post.link)
-                && Objects.equals(description, post.description);
+        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(created, post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, description);
+        return Objects.hash(id, title, link, created);
     }
 
     @Override
