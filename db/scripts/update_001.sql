cbe99c6e62cced2a807d73df7feff09faec408a9
Denis Davydov <dvd9@yandex.ru>
2021-11-19 10:00:27 +0300

    post unique

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -2,6 +2,6 @@ create table if not exists post (
     id serial primary key,
     name varchar(255),
     text text,
-    link varchar(500),
+    link varchar(500) unique,
     created timestamp
 );
