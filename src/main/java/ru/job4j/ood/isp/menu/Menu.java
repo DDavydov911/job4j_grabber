469b15cc2f43f6ae14c6bbafb5a98b528d2d876f
Denis Davydov <dvd9@yandex.ru>
2022-01-19 15:21:35 +0300

    1. Создать меню. [#4748 #249954] 5 days fixed

--- src/main/java/ru/job4j/ood/isp/menu/Menu.java
+++ src/main/java/ru/job4j/ood/isp/menu/Menu.java
@@ -1,93 +1,90 @@
 package ru.job4j.ood.isp.menu;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Menu {
-    private Element firstElement;
+    private List<Element> list;
+    private Element transit;
+    private StringBuilder sb = new StringBuilder();
 
-    public Menu(Element firstElement) {
-        this.firstElement = firstElement;
+    public Menu(List<Element> list) {
+        this.list = list;
+    }
+
+    public Element getTransit() {
+        return transit;
+    }
+
+    public void checkAll(List<Element> list, Predicate<Element> predicate) {
+        for (Element element : list) {
+            if (predicate.test(element)) {
+                transit = element;
+            } else {
+                checkAll(element.getInners(), predicate);
+            }
+        }
+    }
+
+    public Element findElement(String name) {
+        checkAll(list, el -> name.equals(el.getName()));
+        Element result = transit;
+        transit = null;
+        return result;
     }
 
     public boolean add(String patternsName, String childName, Action action) {
         boolean result = false;
-        Element element = findElement(firstElement, patternsName);
-        if (element != null) {
-            result = element.getInners().add(new MultiElem(childName, action));
+        if (patternsName == null) {
+            result = list.add(new MultiElem(childName, action));
+        } else {
+            Element element = findElement(patternsName);
+            if (element != null) {
+                result = element.getInners().add(new MultiElem(childName, action));
+            }
         }
         return result;
     }
 
-    Action select(String itemName) {
-        Element element = findElement(firstElement, itemName);
-        return element.getAction();
+    public Action select(String name) {
+        Element element = findElement(name);
+        return (element != null) ? element.getAction() : null;
     }
 
-    String getAsString() {
-        StringBuilder sb = new StringBuilder();
-        for (Element element : firstElement.getInners()) {
-            sb.append(element.getName());
-        }
-        return sb.toString();
-    }
-
-    public void start() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            this.showMenu(firstElement.getInners());
-            System.out.println("Для выхода введите \"exit\"");
-            String answer = scanner.nextLine();
-            if (answer.equalsIgnoreCase("exit")) {
-                scanner.close();
-                return;
-            }
-            Element element = findElement(firstElement, answer);
-            if (element != null) {
-                element.execute();
-            }
+    public void fillBuilder(List<Element> list) {
+        for (Element element : list) {
+            sb.append(element.getName()).append("\n");
+            fillBuilder(element.getInners());
         }
     }
 
-    private void showMenu(List<Element> elements) {
-        if (elements.equals(firstElement.getInners())) {
-            System.out.println("Menu.");
-        }
-        for (Element element : elements) {
-            System.out.println(element.getName());
-            showMenu(element.getInners());
-        }
-    }
-
-    public Element findElement(Element startElement, String name) {
-        if (name.equalsIgnoreCase(startElement.getName())) {
-            return startElement;
-        } else {
-            List<Element> list = startElement.getInners();
-            if (list != null) {
-                for (Element elements : list) {
-                    return findElement(elements, name);
-                }
-            }
-            return null;
-        }
+    public String getAsString() {
+        fillBuilder(list);
+        String result = sb.toString();
+        sb = new StringBuilder();
+        return result;
     }
 
     public static void main(String[] args) {
+        List<Element> list = new ArrayList<>();
         Element el1 = new MultiElem("1", new PrintAction());
         Element el2 = new MultiElem("2", new CodeAction());
-        el2.add(new MultiElem("2.1", new PrintAction()));
-        el2.add(new MultiElem("2.2", new PrintAction()));
-        Element el3 = new MultiElem("3", new PrintAction());
-        Element el4 = new MultiElem("4", new PrintAction());
-        Element el5 = new MultiElem("5", new PrintAction());
-        Element el0 = new MultiElem("0", new PrintAction());
-        el0.add(el1);
-        el0.add(el2);
-        el0.add(el3);
-        el0.add(el4);
-        el0.add(el5);
-        Menu menu = new Menu(el0);
-        menu.start();
+        list.add(el1);
+        list.add(el2);
+        Element el21 = new MultiElem("2.1", new PrintAction());
+        el2.add(el21);
+        Element el212 = new MultiElem("2.1.2", new PrintAction());
+        el21.add(el212);
+        Element el2121 = new MultiElem("2.1.2.1", new PrintAction());
+        el212.add(el2121);
+        Element el13 = new MultiElem("1.3", new CodeAction());
+        el1.add(el13);
+        Menu menu2 = new Menu(list);
+        Element el3 = new MultiElem("3", new CodeAction());
+        list.add(el3);
+        System.out.println(menu2.add("3", "3.1", new PrintAction()));
+        menu2.checkAll(list, el -> "3.1".equals(el.getName()));
+        System.out.println(menu2.getAsString());
     }
 }
